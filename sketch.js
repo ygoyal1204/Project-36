c50c8206258da004f5e3330d3fffab2d8d9a122c
Yashvi Goyal <72339157+ygoyal1204@users.noreply.github.com>
2020-12-02 22:49:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,85 @@
+var dogImg, happyDogImg
+var dog 
+var database 
+var foodS, foodStock
+var lastFed, fedTime
+var foodObj
+var feed, addFood, food1, foodCount
+var input, milk, milkImg
+
+function preload() {
+  dogImg = loadImage('images/dogImg.png');
+  happyDogImg = loadImage('images/dogImg1.png');
+  milkImg = loadImage('images/Milk.png');
+}
+
+function setup() {
+  database = firebase.database();
+
+  dog = createSprite(650, 250);
+  dog.scale = 0.3;
+  dog.addImage(dogImg);
+
+  milk = createSprite(565, 300);
+  milk.addImage(milkImg);
+  milk.scale = 0.1;
+  milk.visible = false;
+  milk.rotation = 55;
+  
+  food1 = new Food();
+  
+  food1.start();
+
+  addFood = createButton("Add food");
+  addFood.position(655, 165);
+  addFood.mousePressed(addFoods);
+
+  input = createInput("Your Dog's Name");
+  input.position(665, 105);
+
+  feed = createButton("Feed your Dog");
+  feed.position(740, 165);
+  feed.mousePressed(feedDog);
+
+  canvas = createCanvas(800, 400);
+}
+
+function draw() {  
+  background(46, 139, 87);
+
+  fill("white");
+  textSize(25);
+  textAlign(CENTER);
+  text("Virtual Pet 2", 400, 40);
+
+  food1.display();
+
+  drawSprites();
+}
+
+function feedDog() {
+  food1.getFoodStock();
+  food1.updateFedTime();
+
+  if(foodCount === 0) {
+    foodCount = 0;
+    milk.visible = false;
+    dog.addImage(dogImg);
+  } else {
+    food1.updateFoodStock(foodCount - 1);
+    milk.visible = true;
+    dog.addImage(happyDogImg);
+  }
+}
+
+function addFoods() {
+ food1.getFoodStock();
+
+ food1.updateFoodStock(foodCount + 1); 
+}
+
+
+
+
+
+
